1a1c93d9747649ce4df6b06d1dd89e85cfebb44f
dan.cummings <dan.cummings@dynatrace.com>
2022-09-01 20:45:31 -0400

    added Michaels code

--- src/index.ts
+++ src/index.ts
@@ -1,26 +1,29 @@
 class Player {
-    name: string;
-    jersey: number;
+  name: string;
+  jersey: number;
 
-    constructor(name: string, jersey: number){
-        this.name = name;
-        this.jersey = jersey;
-    }
-};
+  constructor(name: string, jersey: number) {
+    this.name = name;
+    this.jersey = jersey;
+  }
+}
 
+class BasketBallPlayer extends Player {
+  slamDunks: number;
+  constructor(name: string, jersey: number, slamDunks: number = 0) {
+    super(name, jersey);
+    this.slamDunks = slamDunks;
+  }
 
-class BasketBallPlayer extends Player{
-    slamDunks:number;
-    constructor(name: string, jersey: number, slamDunks:number = 0){
-        super(name, jersey);
-        this.slamDunks = slamDunks;
-    }
-};
+  increaseSlamDunks() {
+    this.slamDunks++;
+  }
+}
 
-class BaseBallPlayer extends Player{
-    homeRuns:number;
-    constructor(name: string, jersey: number, homeRuns:number = 0){
-        super(name, jersey);
-        this.homeRuns = homeRuns;
-    }
-};
+class BaseBallPlayer extends Player {
+  homeRuns: number;
+  constructor(name: string, jersey: number, homeRuns: number = 0) {
+    super(name, jersey);
+    this.homeRuns = homeRuns;
+  }
+}
